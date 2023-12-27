f089923173f87b52716a4f5ebea613ed727bb952
ankabutdev <Ankabut571@gmail.com>
2023-12-27 18:00:26 +0500

    learn JSON.parse

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-// main.js
-import * as all from './utils.js';
-import { add, subtract } from './math.js';
+// // main.js
+// import * as all from './utils.js';
+// import { add, subtract } from './math.js';
 
-export default all;
+// export default all;
 
 // console.log(all.double(2));
 // console.log(all.square(2));
@@ -12,3 +12,18 @@ export default all;
 
 // const result2 = subtract(10, 4);
 // console.log(result2); // Output: 6
+
+
+let text = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+
+const obj = JSON.parse(text);
+
+console.log(obj);
+
+let x = "Hello";
+x = 5;
+
+console.log(x);
